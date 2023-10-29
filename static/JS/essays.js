5574f2803e0fa405296a5661e569788089569cca
Alex Krysztofiak <128325501+Alex-Krysztofiak@users.noreply.github.com>
2023-10-29 15:06:16 -0400

    Add files via upload

--- static/JS/essays.js
+++ static/JS/essays.js
@@ -1,51 +1,14 @@
-class MyDisplay extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            essaysIndex: 0,
-            essays: [],
-            isLoading: true,
-        }
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
-
-    componentDidMount() {
-        fetch('..\\static\\JSON\\essays.json')
-            .then(response => response.json())
-            .then(data => this.setState({ essays: data, isLoading: false }));
-    }
-
-    render() {
-        if (this.state.isLoading) {
-            return (<p>Loading ....</p>)
-        } else {
-            return (
-
-                <>
-                    <div>
-                        <h1>Essays</h1>
-                        <h4><a href="../index.html">back</a></h4>
-                        {this.state.essays.map(essays => {
-                            return (
-                                <>
-                                    <div className="project">
-                                        <h4 className="title">{essays.essay0['Title']}</h4>
-                                        <p className="textbody">{essays.essay0['Text']}</p>
-                                    </div>
-
-
-                                </>
-                            )
-                        })}
-                        <h4 className="bottomh4"><i><a href="../index.html">back</a></i></h4>
-                    </div>
-
-                </>
-
-            ) //match return in else of isLoading 
-
-        } //match else of isLoading
-    }
-
+  });
 }
-
-ReactDOM.render(<MyDisplay />, document.querySelector('#divDisplay'))
--- static/JS/projects.js
+++ static/JS/projects.js
@@ -1,3 +1,5 @@
+
+
 class MyDisplay extends React.Component {
     constructor(props) {
         super(props)
@@ -48,7 +50,15 @@ class MyDisplay extends React.Component {
                                         <p className="textbody">{projects.Text1}</p>
                                         <p className="textbody">{projects.Text2}</p>
                                         <p className="textbody">{projects.Tech}</p>
-                                        <p className="essaybody">{projects.Text3}</p>
+                                        
+                                            
+                                            <button className="accordion"><p className="textbody">{projects.Text3}</p></button>
+                                            <div className="panel">
+                                            <p className="essaybody">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                            <p className="essaybody">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                            <p className="essaybody">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>  
+                                            </div>
+                                        
                                     </div>
 
 
